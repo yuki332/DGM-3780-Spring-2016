7639e94489dc1f7a8bb6062fbf1fce985e394e4e
Yuki Arai <sk80332@gmail.com>
2016-03-21 22:40:20 -0600

    complete chap8

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -30,7 +30,8 @@ if (Meteor.isClient) {
 
             Tasks.insert({
                 text: text,
-                createdAt: new Date()
+                createdAt: new Date(),
+                owner: Meteor.userId(),           username: Meteor.user().username
             });
 
             event.target.text.value = "";
@@ -49,6 +50,10 @@ if (Meteor.isClient) {
         "click .delete": function(){
             Tasks.remove(this._id);
         }
+    });
+
+    Accounts.ui.config({
+        passwordSignupFields: "USERNAME_ONLY"
     });
 }
 
