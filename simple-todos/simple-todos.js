5cdcc0e6317f3aff5b98c7dbebed6189ffcfe12b
Yuki Arai <sk80332@gmail.com>
2016-03-21 19:18:53 -0600

    complete chap3

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,10 +1,15 @@
+Tasks = new Mongo.Collection("tasks");
+
 if (Meteor.isClient) {
   Template.body.helpers({
-    tasks: [
-      { text: "this is task 1" },
-      { text: "this is task 2" },
-      { text: "this is task 3" },
-    ]
+    tasks: function() {
+        return Tasks.find({});
+    }
+    // tasks: [
+    //   { text: "this is task 1" },
+    //   { text: "this is task 2" },
+    //   { text: "this is task 3" },
+    // ]
   });
 }
 
