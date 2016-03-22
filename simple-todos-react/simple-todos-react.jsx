60d578c338285398226745c87674103fd9ab258d
Yuki Arai <sk80332@gmail.com>
2016-03-21 23:10:21 -0600

    complete meteor with react chap3

--- simple-todos-react/simple-todos-react.jsx
+++ simple-todos-react/simple-todos-react.jsx
@@ -1,8 +1,7 @@
-if (Meteor.isClient) {
-  // This code is executed on the client only
+Tasks = new Mongo.Collection("tasks");
 
-  Meteor.startup(function () {
-    // Use Meteor.startup to render the component after the page is ready
-    React.render(<App />, document.getElementById("render-target"));
-  });
+if (Meteor.isClient) {
+    Meteor.startup(function () {
+        React.render(<App />, document.getElementById("render-target"));
+    });
 }
