6531366fc7de9e577512e600675fbb6aa4237f2c
Yuki Arai <sk80332@gmail.com>
2016-03-21 23:30:47 -0600

    complete meteor with react chap7

--- simple-todos-react/app.jsx
+++ simple-todos-react/app.jsx
@@ -1,10 +1,21 @@
 // App component - represents the whole app
 App = React.createClass({
     mixins: [ReactMeteorData],
-    getMeteorData() {
+    getInitialState() {
         return {
-            tasks: Tasks.find({}, {sort: {createdAt: -1}}).fetch()
+            hideCompleted: false
         }
+    },
+
+    getMeteorData() {
+        let query = {};
+        if (this.state.hideCompleted) {
+            query = {checked: {$ne: true}};
+        }
+        return {
+            tasks: Tasks.find(query, {sort: {createdAt: -1}}).fetch(),
+            incompleteCount: Tasks.find({checked: {$ne: true}}).count()
+        };
     },
 
     renderTasks() {
@@ -24,14 +35,26 @@ App = React.createClass({
         React.findDOMNode(this.refs.textInput).value = "";
     },
 
+    toggleHideCompleted() {
+        this.setState({
+            hideCompleted: ! this.state.hideCompleted
+        });
+    },
+
   render() {
     return (
       <div className="container">
         <header>
-          <h1>Todo List</h1>
-              <form className="new-task" onSubmit={this.handleSubmit} >
-                  <input type="text" ref="textInput" placeholder="Type to add new tasks" />
-              </form>
+            <h1>Todo List ({this.data.incompleteCount})</h1>
+
+            <label className="hide-completed">
+                <input type="checkbox" readOnly={true} checked={this.state.hideCompleted} onClick={this.toggleHideCompleted} />
+                Hide Completed Tasks
+            </label>
+
+            <form className="new-task" onSubmit={this.handleSubmit} >
+                <input type="text" ref="textInput" placeholder="Type to add new tasks" />
+            </form>
         </header>
 
         <ul>
