7e092a84ee091f0438effdb28726c34285c9f4a3
Yuki Arai <sk80332@gmail.com>
2016-03-21 19:51:17 -0600

    complete chap7

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -3,7 +3,17 @@ Tasks = new Mongo.Collection("tasks");
 if (Meteor.isClient) {
     Template.body.helpers({
         tasks: function() {
-            return Tasks.find({}, {sort: {createdAt: -1}});
+            if(Session.get("hideCompleted")){
+                return Tasks.find({checked: {$ne: true}}, {sort: {createdAt: -1}});
+            }else{
+                return Tasks.find({}, {sort: {createdAt: -1}});
+            }
+        },
+        hideCompleted: function(){
+            return Session.get("hideCompleted");
+        },
+        incompleteCount: function(){
+            return Tasks.find({checked: {$ne: true}}).count();
         }
     // tasks: [
     //   { text: "this is task 1" },
@@ -24,6 +34,9 @@ if (Meteor.isClient) {
             });
 
             event.target.text.value = "";
+        },
+        "change .hide-completed input": function(event){
+            Session.set("hideCompleted", event.target.checked);
         }
     });
 
