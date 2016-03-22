157c06f7011a612b737ffe6596fb8c1cd04f3124
Yuki Arai <sk80332@gmail.com>
2016-03-21 19:13:16 -0600

    complete chap2

--- simple-todos/simple-todos.js
+++ simple-todos/simple-todos.js
@@ -1,23 +1,33 @@
 if (Meteor.isClient) {
-  // counter starts at 0
-  Session.setDefault('counter', 0);
-
-  Template.hello.helpers({
-    counter: function () {
-      return Session.get('counter');
-    }
-  });
-
-  Template.hello.events({
-    'click button': function () {
-      // increment the counter when button is clicked
-      Session.set('counter', Session.get('counter') + 1);
-    }
+  Template.body.helpers({
+    tasks: [
+      { text: "this is task 1" },
+      { text: "this is task 2" },
+      { text: "this is task 3" },
+    ]
   });
 }
 
-if (Meteor.isServer) {
-  Meteor.startup(function () {
-    // code to run on server at startup
-  });
-}
+// if (Meteor.isClient) {
+//   // counter starts at 0
+//   Session.setDefault('counter', 0);
+//
+//   Template.hello.helpers({
+//     counter: function () {
+//       return Session.get('counter');
+//     }
+//   });
+//
+//   Template.hello.events({
+//     'click button': function () {
+//       // increment the counter when button is clicked
+//       Session.set('counter', Session.get('counter') + 1);
+//     }
+//   });
+// }
+//
+// if (Meteor.isServer) {
+//   Meteor.startup(function () {
+//     // code to run on server at startup
+//   });
+// }
